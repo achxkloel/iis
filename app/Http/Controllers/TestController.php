65459cedd0989f180ec6827db4e86ffe182a9a67
achxkloel <evgeny.torbin49@gmail.com>
2022-11-20 01:51:53 +0100

    update .gitignore test database

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class TestController
 {
     public function get(): string {
+
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        // });
+        DB::table('person')->insert([
+            'login' => 'test',
+            'birth_number' => '30.11.2000',
+            'name' => "Petr",
+            "surname" => "Peringer"
+        ]);
+        
         return 'Steve';
     }
 }
