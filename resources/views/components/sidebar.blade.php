eeefa7d640c420bf6bbc4896206f9295fb27f006
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-11-21 12:01:31 +0100

    edited sidebar styling

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,9 +1,10 @@
 <div class="sidenav">
+    {{-- TODO: opravneni --}}
     <a href="#"><img src="{{ URL::asset('img/logo.png') }}" alt=""></a>
     @if(!Route::is('login'))
-        <a href="/login">Přihlásit se</a>
+        <a href="{{ route('login') }}">Přihlásit se</a>
     @endif
-    <a href="#">Další odkaz</a>
-    <a href="#">A další</a>
-    <a href="#">A ještě jeden</a>
+    <a href="#">Profil</a>
+    <a href="#">Přehled studia</a>
+    <a href="#">Mé kurzy</a>
 </div>
