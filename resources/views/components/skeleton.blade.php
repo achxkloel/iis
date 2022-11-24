3be2f81823ff64149adfdb49060efd47608ab4e6
achxkloel <evgeny.torbin49@gmail.com>
2022-11-24 21:19:34 +0100

    add jquery and custom js scripts slot

--- resources/views/components/skeleton.blade.php
+++ resources/views/components/skeleton.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>WIS</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet">
@@ -13,5 +14,15 @@
         @endif
         <x-sidebar />
         {{ $slot }}
+
+        <!-- Add jQuery -->
+        <script
+	        src="https://code.jquery.com/jquery-3.6.1.min.js"
+		    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+		    crossorigin="anonymous">
+        </script>
+
+        <!-- Custom js scripts -->
+        {{ $js ?? '' }}
     </body>
 </html>
