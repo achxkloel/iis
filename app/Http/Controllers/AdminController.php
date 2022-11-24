2df159d7e2d81a1b87822779204ac9a7c5154039
achxkloel <evgeny.torbin49@gmail.com>
2022-11-24 21:19:14 +0100

    add person creation

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreatePersonRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
+    // Validation rules for login
+    private $login_rules = 'required|min';
+
     public function showPersons () {
         return view('admin.persons', ['persons' => Person::all()]);
     }
@@ -18,11 +25,52 @@ class AdminController extends Controller
         return view('admin.personCreate');
     }
 
-    public function createNewPerson () {
-        return back();
+    public function createNewPerson (CreatePersonRequest $request) {
+        $data = $request->validated();
+
+        if (Person::where('login', $data['login'])->exists()) {
+            return back()
+                ->withErrors([
+                    'error' => true,
+                ])
+                ->withInput();
+        }
+
+        $password = Str::random(20);
+
+        $person = Person::create([
+            'login' => $data['login'],
+            'password' => Hash::make($password),
+            'name' => $data['name'],
+            'surname' => $data['surname'],
+            'role' => $data['role']
+        ]);
+
+        $person->assignRole($this->getRolesList($data['role']));
+
+        return back()->with([
+            'success' => true,
+            'login' => $data['login'],
+            'password' => $password
+        ]);
     }
 
-    public function checkLogin () {
-        return back();
+    public function checkLogin (Request $request) {
+        return response()->json([ 'msg' => 'test' ]);
+    }
+
+    private function getRolesList (string $role) {
+        $additional_roles = [];
+
+        switch ($role) {
+            case 'guarantor':
+                $additional_roles = ['teacher', 'student'];
+                break;
+            case 'teacher':
+                $additional_roles = ['student'];
+                break;
+        }
+
+        return array_push($additional_roles, $role);
     }
 }
--- resources/views/admin/personCreate.blade.php
+++ resources/views/admin/personCreate.blade.php
@@ -6,30 +6,34 @@
         <form action="{{ route('admin-create-person') }}" method="post">
             @csrf
 
-            @error('alert')
-                <div class="alert alert-danger" role="alert">
-                    {{ $message }}
+            @error('error')
+                <div class="alert alert-danger text-start" role="alert">
+                    Uživatel s loginem <span class="fw-bold">{{ old('login') }}</span> již existuje!
                 </div>
             @enderror
 
-            @error('success')
-                <div class="alert alert-success" role="alert">
-                    {{ $message }}
+            @if (Session::has('success'))
+                <div class="alert alert-success text-start" role="alert">
+                    <h4 class="alert-heading">Uživatel byl úspěšně vytvořen</h4>
+                    <p>Po aktualizaci stranky heslo nebude dostupné!</p>
+                    <hr>
+                    <p class="mb-0">
+                        <div>Login: <span class="fw-bold">{{ Session::get('login') }}</span></div>
+                        <div>Heslo: <span class="fw-bold">{{ Session::get('password') }}</span></div>
+                    </p>
                 </div>
-            @enderror
+            @endif
 
             <div class="mb-3 row">
                 <label for="login" class="col-sm-2 col-form-label required-label">Login</label>
                 <div class="col-sm-10">
-                    <div class="input-group">
-                        <input type="text" class="form-control @error('login') is-invalid @enderror" id="login" name="login" value="{{ old('login') }}" aria-describedby="loginError">
-                        <a href="#" class="btn btn-outline-secondary">Zkontrolovat</a>
-                    </div>
+                    <input type="text" class="form-control @error('login') is-invalid @enderror" id="login" name="login" value="{{ old('login') }}" aria-describedby="loginError">
                     @error('login')
                         <div id="loginError" class="invalid-feedback text-start">
                             {{ $message }}
                         </div>
                     @enderror
+                    <div class="form-text text-start">Login musí obsahovat pouze písmena, čísla a speciální znaky (_, -). Minimální délka je 6.</div>
                 </div>
             </div>
             <div class="mb-3 row">
@@ -47,7 +51,7 @@
                 <label for="surname" class="col-sm-2 col-form-label required-label">Příjmení</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control @error('surname') is-invalid @enderror" id="surname" name="surname" value="{{ old('surname') }}" aria-describedby="surnameError">
-                    @error('login')
+                    @error('surname')
                         <div id="surnameError" class="invalid-feedback text-start">
                             {{ $message }}
                         </div>
@@ -57,10 +61,10 @@
             <div class="mb-3 row">
                 <label for="role" class="col-sm-2 col-form-label required-label">Role</label>
                 <div class="col-sm-10">
-                    <select class="form-select @error('role') is-invalid  @enderror" id="role" name="role" aria-describedby="roleError" >
-                        <option value="student" selected>Student</option>
-                        <option value="teacher">Učitel</option>
-                        <option value="guarantor">Garant</option>
+                    <select class="form-select @error('role') is-invalid  @enderror" id="role" name="role" value="{{ old('role') }}" aria-describedby="roleError" >
+                        <option value="student" {{ old('role') == 'student' ? "selected" : "" }}>Student</option>
+                        <option value="teacher" {{ old('role') == 'teacher' ? "selected" : "" }}>Učitel</option>
+                        <option value="guarantor" {{ old('role') == 'guarantor' ? "selected" : "" }}>Garant</option>
                     </select>
                     @error('role')
                         <div id="roleError" class="invalid-feedback text-start">
