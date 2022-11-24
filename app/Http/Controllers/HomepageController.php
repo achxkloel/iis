10ee24b64d4f2438d37acbed74a85616c8c1c34a
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-11-24 16:41:31 +0100

    added course downloading to pages, later on i will add some joins and stuff like that

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use Illuminate\Http\Request;
 
 class HomepageController
 {
-    public function getAllCourses() {
-        return Course::all();
+    public function getCourses(Request $request) {
+        $courses = Course::all();
+        $fulltextString = $request->query('fulltext');
+
+        $courses = $courses->filter(function ($course) use ($fulltextString) {
+            return str_contains($course->shortcut, $fulltextString) || str_contains($course->name, $fulltextString);
+        });
+
+        return view('homepage', ['courses' => $courses]);
     }
 }
