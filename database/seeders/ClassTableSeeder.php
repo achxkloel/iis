d4514ced4ac21152104e52db538911b636a94c5c
achxkloel <evgeny.torbin49@gmail.com>
2022-11-29 01:17:06 +0100

    fix class types

--- database/seeders/ClassTableSeeder.php
+++ database/seeders/ClassTableSeeder.php
@@ -19,11 +19,11 @@ class ClassTableSeeder extends Seeder
 
     public function createDefaultClassrooms (): void {
         $classes = [
-            ['name' => 'Domácí úloha', 'description' => 'Domaci uloha', 'type' => 'practical', 'capacity' => 0, 'floor' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'A100', 'description' => 'Short description', 'type' => 'practical', 'capacity' => 20, 'floor' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'B100', 'description' => 'Short description', 'type' => 'laboratory', 'capacity' => 10, 'floor' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'C200', 'description' => 'Short description', 'type' => 'auditorium', 'capacity' => 200, 'floor' => 2, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'D300', 'description' => 'Short description', 'type' => 'auditorium', 'capacity' => 100, 'floor' => 3, 'created_at' => now(), 'updated_at' => now()]
+            ['name' => 'Domácí úloha', 'description' => 'Domaci uloha', 'type' => 'lab', 'capacity' => 0, 'floor' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'A100', 'description' => 'Short description', 'type' => 'lab', 'capacity' => 20, 'floor' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'B100', 'description' => 'Short description', 'type' => 'computer_lab', 'capacity' => 10, 'floor' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'C200', 'description' => 'Short description', 'type' => 'lecture_room', 'capacity' => 200, 'floor' => 2, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'D300', 'description' => 'Short description', 'type' => 'lecture_room', 'capacity' => 100, 'floor' => 3, 'created_at' => now(), 'updated_at' => now()]
         ];
 
         Classroom::insert($classes);
